52b90677f911605449e408a7476c61e9ff58ee5e
aburakt <info@aburakt.com>
2025-01-26 16:35:45 +0300

    vercel analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from "@vercel/analytics/react";
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { AuthProvider } from './context/AuthContext';
@@ -15,6 +16,7 @@ const App: React.FC = () => {
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <DataProvider>
+            <Analytics />
             <GlobalStyles />
             <AppRoutes />
           </DataProvider>
